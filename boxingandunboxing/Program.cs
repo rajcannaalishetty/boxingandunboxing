4f75eb33736cb53e686ba20fb24aa408c042c23e
rajcannaalishetty <rajcanna.a@gmail.com>
2017-02-17 17:49:20 -0600

    boxing and unboxing

--- boxingandunboxing/Program.cs
+++ boxingandunboxing/Program.cs
@@ -10,6 +10,27 @@ namespace boxingandunboxing
     {
         static void Main(string[] args)
         {
+            //Console.WriteLine("enter the variable");
+            //string x = (Console.ReadLine());
+
+            int x = 78;
+            int n = (int) GetBox(x);
+            
+            Console.WriteLine("the unboxed variable is {0}",n);
+            Console.ReadLine();
+            
+
         }
+
+        public static object GetBox(object obj)
+        {
+
+            Console.WriteLine("the boxed variable is {0}",obj);
+            return obj;
+            //Console.ReadLine();
+
+
+        }
+
     }
 }
